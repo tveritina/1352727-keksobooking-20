e0d63296a9e9b5d19695b29f15bb5f10799468ab
tveritina <tveritinanastya@gmail.com>
2020-06-10 19:30:33 +0500

    Создание карточки вынесена в функцию

--- js/main.js
+++ js/main.js
@@ -108,44 +108,47 @@ for (var i = 0; i < rentList.length; i++) {
 
 mapPins.appendChild(fragment);
 
-var cardElement = card.cloneNode(true);
+var generateCard = function (rentListElement) {
+  var cardElement = card.cloneNode(true);
 
-cardElement.querySelector('.popup__title').textContent = rentList[0].offer.title;
-cardElement.querySelector('.popup__text--address').textContent = rentList[0].offer.address;
-cardElement.querySelector('.popup__text--price').textContent = rentList[0].offer.price + '₽/ночь';
+  cardElement.querySelector('.popup__title').textContent = rentListElement.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = rentListElement.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = rentListElement.offer.price + '₽/ночь';
+  cardElement.querySelector('.popup__text--capacity').textContent = rentListElement.offer.rooms + ' комнат для ' + rentListElement.offer.guests + ' гостей';
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + rentListElement.offer.checkin + ', выезд до ' + rentListElement.offer.checkout;
+  cardElement.querySelector('.popup__description').textContent = rentListElement.offer.description;
+  cardElement.querySelector('.popup__avatar').src = rentListElement.author.avatar;
 
-cardElement.querySelector('.popup__text--capacity').textContent = rentList[0].offer.rooms + ' комнат для ' + rentList[0].offer.guests + ' гостей';
-cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + rentList[0].offer.checkin + ', выезд до ' + rentList[0].offer.checkout;
-cardElement.querySelector('.popup__description').textContent = rentList[0].offer.description;
-cardElement.querySelector('.popup__avatar').src = rentList[0].author.avatar;
+  cardElement.querySelector('.popup__features').innerHTML = '';
+  for (i = 0; i < rentListElement.offer.features.length; i++) {
+    var feature = '<li class="popup__feature popup__feature--' + rentListElement.offer.features[i] + '"></li>';
+    cardElement.querySelector('.popup__features').insertAdjacentHTML('beforeend', feature);
+  }
 
-cardElement.querySelector('.popup__features').innerHTML = '';
+  cardElement.querySelector('.popup__photos').innerHTML = '';
+  for (i = 0; i < rentListElement.offer.photos.length; i++) {
+    var photo = '<img src="' + rentListElement.offer.photos[i] + '" class="popup__photo" width="45" height="40" alt="Фотография жилья">';
+    cardElement.querySelector('.popup__photos').insertAdjacentHTML('beforeend', photo);
+  }
 
-for (i = 0; i < rentList[0].offer.features.length; i++) {
-  var feature = '<li class="popup__feature popup__feature--' + rentList[0].offer.features[i] + '"></li>';
-  cardElement.querySelector('.popup__features').insertAdjacentHTML('beforeend', feature);
-}
+  switch (rentListElement.offer.type) {
+    case ('flat'):
+      cardElement.querySelector('.popup__type').textContent = 'Квартира';
+      break;
+    case ('bungalo'):
+      cardElement.querySelector('.popup__type').textContent = 'Бунгало';
+      break;
+    case ('house'):
+      cardElement.querySelector('.popup__type').textContent = 'Дом';
+      break;
+    case ('palace'):
+      cardElement.querySelector('.popup__type').textContent = 'Дворец';
+      break;
+  }
 
-cardElement.querySelector('.popup__photos').innerHTML = '';
+  mapPins.insertAdjacentElement('afterend', cardElement);
+};
 
-for (i = 0; i < rentList[0].offer.photos.length; i++) {
-  var photo = '<img src="' + rentList[0].offer.photos[i] + '" class="popup__photo" width="45" height="40" alt="Фотография жилья">';
-  cardElement.querySelector('.popup__photos').insertAdjacentHTML('beforeend', photo);
-}
+generateCard(rentList[0]);
 
-switch (rentList[0].offer.type) {
-  case ('flat'):
-    cardElement.querySelector('.popup__type').textContent = 'Квартира';
-    break;
-  case ('bungalo'):
-    cardElement.querySelector('.popup__type').textContent = 'Бунгало';
-    break;
-  case ('house'):
-    cardElement.querySelector('.popup__type').textContent = 'Дом';
-    break;
-  case ('palace'):
-    cardElement.querySelector('.popup__type').textContent = 'Дворец';
-    break;
-}
 
-mapPins.insertAdjacentElement('afterend', cardElement);
