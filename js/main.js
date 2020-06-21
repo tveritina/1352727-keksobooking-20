576bcaafce1e5c1633781a2d6d28e1b026fe4591
tveritina <tveritinanastya@gmail.com>
2020-06-21 12:35:04 +0500

    Карта и валидации вынесены в модули

--- js/main.js
+++ js/main.js
@@ -1,48 +1,18 @@
 'use strict';
 
-var MIN_TITLE_LENGTH = 30;
-var MAX_TITLE_LENGTH = 100;
-var MAX_PRICE_VALUE = 1000000;
-
-var mapPins = document.querySelector('.map__pins');
-
-
-var activateMap = function () {
-  var map = document.querySelector('.map');
-  map.classList.remove('map--faded');
-};
-
-
-
-// Активация карты
+// Активация страницы
 var activatePage = function () {
-  activateMap();
+  window.activateMap();
   window.generateOffersPins();
   window.addActivePinListener();
 };
-
-// Блокировка фильтра
-var blockMapFilter = function (state) {
-  var mapFilter = document.querySelectorAll('.map__filters .map__filter');
-  var mapFeaturesFilter = document.querySelector('.map__filters .map__features');
-
-  for (var i = 0; i < mapFilter.length; i++) {
-    mapFilter[i].disabled = state;
-  }
-
-  mapFeaturesFilter.disabled = state;
-};
-
-blockMapFilter(true);
-
-
 
 // Активация страницы и разблокировка
 var mainMapPin = document.querySelector('.map__pin--main');
 mainMapPin.addEventListener('mousedown', function (evt) {
   if (evt.button === 0) {
     activatePage();
-    blockMapFilter(false);
+    window.blockMapFilter(false);
     window.blockFormFilter(false);
     window.fillAddress();
     window.activateForm();
@@ -52,123 +22,9 @@ mainMapPin.addEventListener('mousedown', function (evt) {
 mainMapPin.addEventListener('keydown', function (evt) {
   if (evt.code === 'Enter') {
     activatePage();
-    blockMapFilter(false);
+    window.blockMapFilter(false);
     window.blockFormFilter(false);
     window.fillAddress();
     window.activateForm();
   }
 });
-
-
-// Валидация количества гостей
-var roomsSelect = document.querySelector('#room_number');
-var capacitySelect = document.querySelector('#capacity');
-
-capacitySelect.addEventListener('change', function () {
-  validateGuests();
-});
-
-roomsSelect.addEventListener('change', function () {
-  validateGuests();
-});
-
-var validateGuests = function () {
-  var capacityValue = parseInt(capacitySelect.value, 10);
-  var roomsValue = parseInt(roomsSelect.value, 10);
-
-  if (capacityValue > roomsValue) {
-    capacitySelect.setCustomValidity('Гостям будет тесно, выберите меньшее количество гостей');
-  } else if (roomsValue === 100 && capacityValue > 0 || roomsValue !== 100 && capacityValue === 0) {
-    capacitySelect.setCustomValidity('Для 100 комнат доступен только вариант "Не для гостей"');
-  } else {
-    capacitySelect.setCustomValidity('');
-  }
-};
-
-
-
-
-
-
-
-// Валидация заголовка
-var offerTitle = document.querySelector('#title');
-
-offerTitle.addEventListener('input', function () {
-  var valueLength = offerTitle.value.length;
-
-  if (valueLength < MIN_TITLE_LENGTH) {
-    offerTitle.setCustomValidity('Ещё ' + (MIN_TITLE_LENGTH - valueLength) + ' симв.');
-  } else if (valueLength > MAX_TITLE_LENGTH) {
-    offerTitle.setCustomValidity('Удалите лишние ' + (valueLength - MAX_TITLE_LENGTH) + ' симв.');
-  } else {
-    offerTitle.setCustomValidity('');
-  }
-});
-
-// Валидация цены
-var offerPrice = document.querySelector('#price');
-
-offerPrice.addEventListener('invalid', function () {
-  if (offerPrice.value.length === 0) {
-    offerPrice.setCustomValidity('Введите число');
-  } else {
-    offerPrice.setCustomValidity('');
-  }
-});
-
-offerPrice.addEventListener('input', function () {
-  var parsedValue = parseInt(offerPrice.value, 10);
-
-  if (isNaN(parsedValue) === true) {
-    offerPrice.setCustomValidity('Введите число');
-  } else if (parsedValue > MAX_PRICE_VALUE) {
-    offerPrice.setCustomValidity('Значение не может быть больше 1 000 000');
-  } else {
-    offerPrice.setCustomValidity('');
-  }
-});
-
-
-// Валидация цены в зависимости от типа жилья
-var homeType = document.querySelector('#type');
-
-homeType.addEventListener('change', function () {
-  validatePriceByHomeType();
-});
-
-offerPrice.addEventListener('input', function () {
-  validatePriceByHomeType();
-});
-
-var validatePriceByHomeType = function () {
-  var typeValue = homeType.value;
-
-  switch (typeValue) {
-    case 'palace':
-      offerPrice.placeholder = '10000';
-      break;
-
-    case 'house':
-      offerPrice.placeholder = '5000';
-      break;
-
-    case 'flat':
-      offerPrice.placeholder = '1000';
-      break;
-
-    case 'bungalo':
-      offerPrice.placeholder = '0';
-      break;
-  }
-
-  if (typeValue === 'palace' && offerPrice.value < 10000) {
-    offerPrice.setCustomValidity('Минимальная цена 10 000');
-  } else if (typeValue === 'house' && offerPrice.value < 5000) {
-    offerPrice.setCustomValidity('Минимальная цена 5 000');
-  } else if (typeValue === 'flat' && offerPrice.value < 1000) {
-    offerPrice.setCustomValidity('Минимальная цена 1 000');
-  } else {
-    offerPrice.setCustomValidity('');
-  }
-};
--- js/map.js
+++ js/map.js
@@ -1,1 +1,22 @@
 'use strict';
+
+(function () {
+  window.activateMap = function () {
+    var map = document.querySelector('.map');
+    map.classList.remove('map--faded');
+  };
+
+  // Блокировка фильтра
+  window.blockMapFilter = function (state) {
+    var mapFilter = document.querySelectorAll('.map__filters .map__filter');
+    var mapFeaturesFilter = document.querySelector('.map__filters .map__features');
+
+    for (var i = 0; i < mapFilter.length; i++) {
+      mapFilter[i].disabled = state;
+    }
+
+    mapFeaturesFilter.disabled = state;
+  };
+
+  blockMapFilter(true);
+})();
