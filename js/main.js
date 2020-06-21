c7c47cac3599e97173260f31ac3a7a4d38b850d2
tveritina <tveritinanastya@gmail.com>
2020-06-21 12:36:39 +0500

    main.js обёрнута в функцию

--- js/main.js
+++ js/main.js
@@ -1,30 +1,33 @@
 'use strict';
 
-// Активация страницы
-var activatePage = function () {
-  window.activateMap();
-  window.generateOffersPins();
-  window.addActivePinListener();
-};
+(function () {
+  // Активация страницы
+  var activatePage = function () {
+    window.activateMap();
+    window.generateOffersPins();
+    window.addActivePinListener();
+  };
 
-// Активация страницы и разблокировка
-var mainMapPin = document.querySelector('.map__pin--main');
-mainMapPin.addEventListener('mousedown', function (evt) {
-  if (evt.button === 0) {
-    activatePage();
-    window.blockMapFilter(false);
-    window.blockFormFilter(false);
-    window.fillAddress();
-    window.activateForm();
-  }
-});
+  // Активация страницы и разблокировка
+  var mainMapPin = document.querySelector('.map__pin--main');
 
-mainMapPin.addEventListener('keydown', function (evt) {
-  if (evt.code === 'Enter') {
-    activatePage();
-    window.blockMapFilter(false);
-    window.blockFormFilter(false);
-    window.fillAddress();
-    window.activateForm();
-  }
-});
+  mainMapPin.addEventListener('mousedown', function (evt) {
+    if (evt.button === 0) {
+      activatePage();
+      window.blockMapFilter(false);
+      window.blockFormFilter(false);
+      window.fillAddress();
+      window.activateForm();
+    }
+  });
+
+  mainMapPin.addEventListener('keydown', function (evt) {
+    if (evt.code === 'Enter') {
+      activatePage();
+      window.blockMapFilter(false);
+      window.blockFormFilter(false);
+      window.fillAddress();
+      window.activateForm();
+    }
+  });
+})();
