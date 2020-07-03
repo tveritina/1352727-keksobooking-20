127da92d02fd7fd571d441d049713460355930f1
tveritina <tveritinanastya@gmail.com>
2020-07-03 15:04:07 +0500

    Добавлены селекторы фильтров и общая функция

--- js/filter.js
+++ js/filter.js
@@ -1,8 +1,29 @@
 'use strict';
 
 (function () {
+  var priceRange = {
+    type: {
+      low: 'low',
+      middle: 'middle',
+      high: 'high',
+    },
+    value: {
+      min: 10000,
+      max: 50000,
+    },
+  };
+
   var filteredOffers = [];
-  var houseTypeFilter = document.querySelector('#housing-type');
+  var filter = document.querySelector('.map__filters');
+  var houseTypeSelect = document.querySelector('#housing-type');
+  var priceSelect = document.querySelector('#housing-price');
+  var roomsSelect = document.querySelector('#housing-rooms');
+  var guestsSelect = document.querySelector('#housing-guests');
+  var featuresFieldset = document.querySelector('#housing-features');
+
+  var filterItems = function (item, offer, key) {
+    return item.value === 'any' ? true : item.value === offer[key].toString();
+  };
 
   var filterByType = function () {
     var typeValue = houseTypeFilter.value;
